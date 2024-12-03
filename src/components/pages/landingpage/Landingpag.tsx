0b27ab1d96df69815f1fc09e1d931e4caf757da2
Rajkumar R <ravindran.rajkumar@vectramind.com>
2024-12-03 12:39:30 +0530

    Added maquee tag on banner
    adde marque tag with banner

--- src/components/pages/landingpage/Landingpag.tsx
+++ src/components/pages/landingpage/Landingpag.tsx
@@ -1,21 +1,102 @@
-import React from "react";
 import Navbar from "../../landingpage/Navbar.tsx";
 import { Button, Typography } from "@mui/material";
-import PersonIcon from '@mui/icons-material/Person';
+import PersonIcon from "@mui/icons-material/Person";
+import Marquee from "@/components/ui/marquee.tsx";
+import { cn } from "@/lib/utils";
+
+const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://avatar.vercel.sh/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://avatar.vercel.sh/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://avatar.vercel.sh/john",
+  },
+];
+
+const firstRow = reviews.slice(0, reviews.length / 2);
+const secondRow = reviews.slice(reviews.length / 2);
+
+const ReviewCard = ({
+  img,
+  name,
+  username,
+  body,
+}: {
+  img: string;
+  name: string;
+  username: string;
+  body: string;
+}) => {
+  return (
+    <figure
+      className={cn(
+        "relative h-40 w-36 cursor-pointer overflow-hidden rounded-xl border p-4",
+        // light styles
+        "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
+        // dark styles
+        "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
+      )}
+    >
+      <div className="flex flex-row items-center gap-2">
+        <img className="rounded-full" width="32" height="32" alt="" src={img} />
+        <div className="flex flex-col">
+          <figcaption className="text-sm font-medium dark:text-white">
+            {name}
+          </figcaption>
+          <p className="text-xs font-medium dark:text-white/40">{username}</p>
+        </div>
+      </div>
+      <blockquote className="mt-2 text-sm">{body}</blockquote>
+    </figure>
+  );
+};
+
 const Landingpag = () => {
   return (
     <div className="main-landingpage">
       <Navbar />
       <section className="banner-section">
         <div className="text-container">
-          <p className="header"><span className="highlighter">BullsCatch</span> Trading Floor</p>
-          <Typography variant="subtitle2" className="secondary-text">learning And earning without risk to personal capital</Typography>
+          <p className="header">
+            <span className="highlighter">BullsCatch</span> Trading Floor
+          </p>
+          <Typography variant="subtitle2" className="secondary-text">
+            learning And earning without risk to personal capital
+          </Typography>
           <div className="banner-button">
-            <Button variant="contained" className="explore-btn">Lets's Explore</Button>
-            <Button startIcon={<PersonIcon/>}  className="contactbtn">Contact</Button>
+            <Button variant="contained" className="explore-btn">
+              Lets's Explore
+            </Button>
+            <Button startIcon={<PersonIcon />} className="contactbtn">
+              Contact
+            </Button>
           </div>
         </div>
-        <img src="" alt="banner-image"/>
+        {/* <img src="" alt="banner-image"/> */}
+        <div className="marqueediv">
+          <Marquee pauseOnHover className="[--duration:20s]">
+            {firstRow.map((review) => (
+              <ReviewCard key={review.username} {...review} />
+            ))}
+          </Marquee>
+          <Marquee reverse pauseOnHover className="[--duration:20s]">
+            {secondRow.map((review) => (
+              <ReviewCard key={review.username} {...review} />
+            ))}
+          </Marquee>
+        </div>
       </section>
     </div>
   );
